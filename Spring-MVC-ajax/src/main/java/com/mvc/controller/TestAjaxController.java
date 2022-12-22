b615ad28a91d2b50f3c9caebbba289f65471d406
Zilong WU <95653923+littlewuuu@users.noreply.github.com>
2022-12-22 16:43:52 +0100

    Test return json data

--- Spring-MVC-ajax/src/main/java/com/mvc/controller/TestAjaxController.java
+++ Spring-MVC-ajax/src/main/java/com/mvc/controller/TestAjaxController.java
@@ -3,9 +3,14 @@ package com.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class TestAjaxController {
@@ -18,7 +23,7 @@ public class TestAjaxController {
     }
 
     /**
-     * s使用 @requestBody 把 json 转化为java 对象
+     * 使用 @requestBody 把 json 转化为java 对象
      * @param httpServletResponse
      * @throws IOException
      */
@@ -27,4 +32,42 @@ public class TestAjaxController {
         System.out.println(user);
         httpServletResponse.getWriter().write("hello requestbody");
     }
+
+
+    /**
+     * 返回 java 对象 -> json 对象
+     * @return
+     * @throws IOException
+     */
+//    @RequestMapping("/test/responseBody/json")
+//    @ResponseBody
+//    public User testResponseBody() throws IOException {//因为这里是 ajax，是局部刷新，不用重新访问页面，所以不用返回 String
+//        return new User(12,"1234","ininin",12,"male");
+//    }
+
+    //响应浏览器list集合
+    @RequestMapping("/test/responseBody/json")
+    @ResponseBody
+    public List<User> testResponseBodyList(){
+        User user1 = new User(1001,"admin1","123456",23,"男");
+        User user2 = new User(1002,"admin2","123456",23,"男");
+        User user3 = new User(1003,"admin3","123456",23,"男");
+        List<User> list = Arrays.asList(user1, user2, user3);
+        return list;
+    }
+
+//    //响应浏览器map集合
+//    @RequestMapping("/test/ResponseBody/json")
+//    @ResponseBody
+//    public Map<String, Object> testResponseBody(){
+//        User user1 = new User(1001,"admin1","123456",23,"男");
+//        User user2 = new User(1002,"admin2","123456",23,"男");
+//        User user3 = new User(1003,"admin3","123456",23,"男");
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("1001", user1);
+//        map.put("1002", user2);
+//        map.put("1003", user3);
+//        return map;
+//    }
 }
+
